e2d4bc5eca415cd23292985e4d5b5ea5ef867d34
ddalcero <ddalcero@gmail.com>
2014-11-27 19:08:29 -0300

    Cambio email

--- application/controllers/facturar.php
+++ application/controllers/facturar.php
@@ -54,6 +54,9 @@ class Facturar_Controller extends Base_Controller {
 			$message = Swift_Message::newInstance('Petición de facturación')
 			  ->setFrom(array('noreply@siigroup.cl' => 'SELMA'))
 			  ->setTo(array(
+			  	  'facturacion@siigroup.cl'
+			  	  ))
+			  ->setCc(array(
 			  	  $email=>$username
 			  	  ))
 			  ->setBody($vista->render(),'text/html');
